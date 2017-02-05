07ac1a1f5dd3034d98cfe8dd883c1075e9ca668b
ey1031 <eyudeveloper@gmail.com>
2017-02-04 20:41:22 -0800

    what am i doing

---
+++ asdf/FirstUIController.swift
@@ -0,0 +1,39 @@
+//
+//  FirstUIController.swift
+//  asdf
+//
+//  Created by Emily on 2/4/17.
+//  Copyright © 2017 Emilyasdf. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class FirstUIController: UIViewController {
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var color1 = hexStringToUIColor(hex: "#d9d9d9")
+        self.view.backgroundColor = color1
+    }
+}
--- asdf/ViewController.swift
+++ asdf/ViewController.swift
@@ -15,10 +15,34 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     var imagepickedtbh:UIImage!
     
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var color1 = hexStringToUIColor(hex: "#d9d9d9")
+        self.view.backgroundColor = color1
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
